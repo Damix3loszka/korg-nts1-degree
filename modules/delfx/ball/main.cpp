5f9e48f1922cccfffe5b6552d8d3479d887ba260
Damix3loszka <DamianDerp@gmail.com>
2023-09-19 21:57:37 +0200

    Rewritten previous commit in terms of DualDelayLine

--- modules/delfx/ball/main.cpp
+++ modules/delfx/ball/main.cpp
@@ -1,61 +1,60 @@
-#include "userdelfx.h"
 
+#include "dsp/delayline.hpp"
+#include "userdelfx.h"
+#include <cmath>
 #define k_samplerate 48000
 #define LINE_LENGTH 65536
-#define LINE_MASK LINE_LENGTH - 1
 
-unsigned int write_index = 0;
-unsigned int read_index = LINE_MASK - k_samplerate;
+__sdram f32pair_t delay_arr[LINE_LENGTH] = {};
 
-__sdram float delay_l[LINE_LENGTH] = {};
-__sdram float delay_r[LINE_LENGTH] = {};
+dsp::DualDelayLine delay_line;
+float feedback = 0.5f;
+float offset_magnitude = 1.f;
+float mix = 0.5f;
 void DELFX_INIT(uint32_t platform, uint32_t api)
 {
+    delay_line.setMemory(delay_arr, LINE_LENGTH);
 }
 
 void DELFX_PROCESS(float *xn, uint32_t frames)
 {
     float *__restrict in = xn;
-    float samples = frames * 2;
-    for (uint32_t i = 0; i < frames; ++i)
+
+    for (uint8_t i = 0; i < frames; ++i)
     {
+
         float in_l_sample = in[2 * i];
         float in_r_sample = in[2 * i + 1];
 
-        float delayed_l_sample = delay_l[read_index];
-        float delayed_r_sample = delay_r[read_index];
+        f32pair_t in_lr_samples = f32pair_t{in_l_sample, in_r_sample};
 
-        delay_l[write_index] = in_l_sample + 0.3f * delayed_l_sample;
-        delay_r[write_index] = in_r_sample + 0.3f * delayed_r_sample;
+        f32pair_t delayed_lr_samples = delay_line.readFrac(k_samplerate * offset_magnitude);
 
-        write_index = ++write_index & LINE_MASK;
-        read_index = ++read_index & LINE_MASK;
+        delay_line.write(f32pair_add(in_lr_samples, f32pair_mulscal(delayed_lr_samples, feedback)));
 
-        in[2 * i] = in_l_sample * 0.3f + delayed_l_sample * 0.7f;
-        in[2 * i + 1] = in_r_sample * 0.3f + delayed_r_sample * 0.7f;
+        float out_l_sample = in_lr_samples.a * (1.f - mix) + delayed_lr_samples.a * mix;
+        float out_r_sample = in_lr_samples.b * (1.f - mix) + delayed_lr_samples.b * mix;
+        in[2 * i] = out_l_sample;
+        in[2 * i + 1] = out_r_sample;
     }
 }
 
 void DELFX_PARAM(uint8_t index, int32_t value)
 {
     const float valf = q31_to_f32(value);
-    const float sample_offest = k_samplerate * (1.f - valf);
+
     switch (index)
     {
-    case k_user_delfx_param_time:
-
-        read_index = write_index - sample_offest;
-        if (read_index < 0)
-        {
-            read_index += LINE_MASK;
-        }
+    case k_user_delfx_param_time:;
+        offset_magnitude = valf;
         break;
 
     case k_user_delfx_param_depth:
+        feedback = valf * 0.7f + 0.1f;
         break;
 
     case k_user_delfx_param_shift_depth:
-
+        mix = valf;
         break;
 
     default:
